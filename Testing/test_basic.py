805cbef6316607e6c9348399af4b484036e96d08
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-06-27 16:53:06 +0500

    one more test for algorithms.

--- Testing/test_basic.py
+++ Testing/test_basic.py
@@ -8,3 +8,11 @@ def test_div_2():
 
 def test_div_3():
     assert div(1, 2) == 0.5
+
+from basicmath import sortlist
+
+def test_list():
+    l = [12, 45, 77, 9, 8, 2, 1]
+    sortetd_list = [1, 2, 8, 9, 12, 45, 77]
+
+    assert sortlist(l) == sortetd_list
