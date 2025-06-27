a185fcb6491ca2361ecd0770a31e233a8dda1534
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-06-27 15:49:58 +0500

    Testing in production.

---
+++ Testing/basicmath.py
@@ -0,0 +1,4 @@
+def div(n1, n2):
+    return n1 / n2
+
+print(div(4, 2))
---
+++ Testing/test_basic.py
@@ -0,0 +1,10 @@
+from basicmath import div
+
+def test_div():
+    assert div(4, 2) == 2 
+
+def test_div_2():
+    assert div(15, 3) == 5
+
+def test_div_3():
+    assert div(1, 2) == 0.5
