9f51912abc04070337cc4c0c0769185da4efff72
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2025-06-28 16:46:17 +0500

    Debugging in python.

---
+++ debugging.py
@@ -0,0 +1,23 @@
+def sqrt(x, guess = 0.0):
+    if x < 0:
+        return None
+    if good_enough(guess, x):
+        return guess
+    else:
+        new_guess = improve_guess(guess, x)
+        return sqrt(x, new_guess)
+
+def good_enough(guess, x):
+    if abs(guess * guess - x) < 0.1:
+        return True
+    else:
+        return False
+
+def improve_guess(guess, x):
+    if guess < x:
+        return guess + 0.1
+    else:
+        return guess - 0.1
+
+print(sqrt(36))
+# print(sqrt(-2))        
